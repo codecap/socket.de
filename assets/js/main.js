dbf3dfc75a501d5db8aa95e6bd24dfa4e87ffd76
Vladislav Nazarenko <vnazarenko@codeberry.de>
2021-11-19 11:34:29 +0100

    tmp

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,16 @@ jQuery(document).ready(function($){
 // Google map.
 function initMap(){
   // The location of Uluru
-    const uluru = { lat: 39.7219538, lng: -91.5098444 };
+    const sl = { lat: 50.084265, lng: 8.661599 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 15,
+      center: sl,
     });
     // The marker, positioned at Uluru
     const iconFolder = 'assets/img/';
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: sl,
       map: map,
       icon: `${iconFolder}location.svg`,
     });
